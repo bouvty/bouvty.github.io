7962ff0a221ab7b8b661dc3d042ecba786be6763
/yau <113791793+bouvty@users.noreply.github.com>
2023-10-30 17:52:14 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const textElement = document.getElementById('text');
+const sentences = [
+  "join discord.gg/fastly",
+  "developer in python, lua",
+  "yau on top",
+  "yau.bio",
+  "panel.fastly.vip"
+];
+
+let sentenceIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+let cursor = true;
+
+function typeWriter() {
+  const currentSentence = sentences[sentenceIndex];
+
+  if (!isDeleting && charIndex <= currentSentence.length) {
+    textElement.textContent = currentSentence.substring(0, charIndex);
+    charIndex++;
+  } else {
+    isDeleting = true;
+    textElement.textContent = currentSentence.substring(0, charIndex);
+    charIndex--;
+
+    if (charIndex === 0) {
+      isDeleting = false;
+      sentenceIndex = (sentenceIndex + 1) % sentences.length;
+    }
+  }
+
+  let cursorSymbol = cursor ? '_' : '_'; 
+  textElement.textContent += cursorSymbol; 
+
+  cursor = !cursor; 
+
+  const speed = isDeleting ? 50 : 20; 
+  const pause = isDeleting ? 100 : 300;
+
+  setTimeout(typeWriter, isDeleting ? speed : pause);
+}
+
+typeWriter();
