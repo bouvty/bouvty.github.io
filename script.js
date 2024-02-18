3d25f5e59bc78c34a7a8a8519759baead8cac599
/yau <113791793+bouvty@users.noreply.github.com>
2024-02-18 18:22:31 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const textElement = document.getElementById('text');
+const sentences = [
+  "betray.vip",
+  "im not termed.",
+  "im just better",
+  "Programmer in python, html and lua ",
+  "betray.vip/invite"
+];
+
+let sentenceIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+let cursor = true;
+
+function typeWriter() {
+  const currentSentence = sentences[sentenceIndex];
+
+  if (!isDeleting && charIndex <= currentSentence.length) {
+    textElement.textContent = currentSentence.substring(0, charIndex);
+    charIndex++;
+  } else {
+    isDeleting = true;
+    textElement.textContent = currentSentence.substring(0, charIndex);
+    charIndex--;
+
+    if (charIndex === 0) {
+      isDeleting = false;
+      sentenceIndex = (sentenceIndex + 1) % sentences.length;
+    }
+  }
+
+  let cursorSymbol = cursor ? '_' : '_'; 
+  textElement.textContent += cursorSymbol; 
+
+  cursor = !cursor; 
+
+  const speed = isDeleting ? 50 : 20; 
+  const pause = isDeleting ? 100 : 300;
+
+  setTimeout(typeWriter, isDeleting ? speed : pause);
+}
+
+typeWriter();
